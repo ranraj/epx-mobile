b9a3ae2955adacc9777b786c211f1a4f49307381
ranjithraj.d <randhana@cisco.com>
2021-08-18 00:25:14 +0530

    video link id

--- src/components/ChapterDetail.js
+++ src/components/ChapterDetail.js
@@ -4,22 +4,26 @@ import YoutubePlayer from 'react-native-youtube-iframe';
 
 const ChapterDetail = ({ chapter }) => {
   //TODO : Move this logic to Backend
-  const videoLinkArray = chapter.videoLink.split('/');
-  const videoLink = videoLinkArray[videoLinkArray.length-1];
-  
+  console.log(chapter.content);
+
   return (
     <>
-    <View style={styles.container}>
+      <View style={styles.container}>
 
-      <Text style={styles.name}>{chapter.name}</Text>
-      <YoutubePlayer style={styles.player}   
-        height={200}     
-        play={false}
-        videoId={videoLink}
-      />
-      <Text style={styles.title}>{chapter.title}</Text>
-      <Button style={styles.button} title="Try Test"></Button>
-    </View>
+        <Text style={styles.name}>{chapter.name}</Text>
+        {
+          chapter.content ?
+          <YoutubePlayer style={styles.player}
+            height={200}
+            play={false}
+            videoId={chapter.content.youTubeVideoId}
+          />
+          :
+          <Text>No Video content available</Text>
+        }
+        <Text style={styles.title}>{chapter.title}</Text>
+        <Button style={styles.button} title="Try Test"></Button>
+      </View>
     </>
   );
 };
@@ -28,11 +32,11 @@ const styles = StyleSheet.create({
   container: {
     marginBottom: 10,
     flex: 1,
-    margin : 8,    
-    borderWidth:  1,        
+    margin: 8,
+    borderWidth: 1,
   },
-  player: {    
-    borderRadius : 4    
+  player: {
+    borderRadius: 4
   },
   image: {
     width: 250,
@@ -44,10 +48,10 @@ const styles = StyleSheet.create({
     fontWeight: 'bold'
   },
   title: {
-    margin:5
+    margin: 5
   },
-  button:{
-    margin:5
+  button: {
+    margin: 5
   }
 
 });
